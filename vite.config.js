e81e4c7fb2cb7d0eb90e86d237de45f906bf8152
cdgalpha <prityelina101@gmail.com>
2026-03-30 05:17:48 +0600

    fix buffer polyfill

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': {},
-    global: 'globalThis',
-  },
+  plugins: [
+    react(),
+    nodePolyfills(),
+  ],
 })
